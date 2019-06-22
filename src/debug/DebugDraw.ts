01eb2cb3f2ac921bd90b124c2f9b91e26cc2e18c
yetiecheng <yetiecheng@100tal.com>
2019-06-22 14:17:44 +0800

    feat: 添加胶囊形状

--- src/debug/DebugDraw.ts
+++ src/debug/DebugDraw.ts
@@ -3,7 +3,10 @@ enum Color {
     BOX = 0xfcaf17,
     PLANE = 0xffd400,
     FILL = 0x7f7522,
-    LINE = 0xf05b72
+    LINE = 0xf05b72,
+    CIRCLE = 0x7fb80e,
+    PARTICLE = 0x2a5caa,
+    CAPSULE = 0x33a3dc
 }
 
 class DebugDraw {
@@ -52,6 +55,10 @@ class DebugDraw {
             this.createCircle(shape, body);
         } else if (shape instanceof p2.Line) {
             this.createLine(shape, body);
+        } else if (shape instanceof p2.Particle) {
+            this.createParticle(shape, body);
+        } else if (shape instanceof p2.Capsule) {
+            this.createCapsule(shape, body);
         }
     }
 
@@ -68,8 +75,8 @@ class DebugDraw {
 
     private createBox(shape, body) {
         this._shape = new egret.Shape();
-        this._shape.graphics.lineStyle(2, Color.BOX);
-        this._shape.graphics.beginFill(Color.FILL, 1);
+        this._shape.graphics.lineStyle(2, Color.FILL);
+        this._shape.graphics.beginFill(Color.BOX, 1);
         this._shape.graphics.moveTo(((-shape.width / 2) * this._factor), ((-shape.height / 2) * this._factor));
         this._shape.graphics.lineTo(((shape.width / 2) * this._factor), ((-shape.height / 2) * this._factor));
         this._shape.graphics.lineTo(((shape.width / 2) * this._factor), ((shape.height / 2) * this._factor));
@@ -83,8 +90,8 @@ class DebugDraw {
 
     private createCircle(shape, body) {
         this._shape = new egret.Shape();
-        this._shape.graphics.lineStyle(2, Color.BOX);
-        this._shape.graphics.beginFill(Color.FILL, 1);
+        this._shape.graphics.lineStyle(1, Color.FILL);
+        this._shape.graphics.beginFill(Color.CIRCLE, 1);
         this._shape.graphics.drawCircle(0, 0, shape.radius * this._factor);
         this._shape.graphics.endFill();
         body.position[0] /= this._factor;
@@ -95,10 +102,38 @@ class DebugDraw {
 
     private createLine(shape, body) {
         this._shape = new egret.Shape();
-        this._shape.graphics.lineStyle(5, Color.LINE);
-        this._shape.graphics.beginFill(Color.FILL, 1);
+        this._shape.graphics.lineStyle(1, Color.LINE);
+        this._shape.graphics.beginFill(Color.LINE, 1);
         this._shape.graphics.drawRect(0, 0, shape.length * this._factor, 2);
         this._shape.graphics.endFill();
+        this._shape.anchorOffsetX = this._shape.width / 2;
+        this._shape.anchorOffsetY = this._shape.height / 2;
+        body.position[0] /= this._factor;
+        body.position[1] /= this._factor;
+        body.displays = [this._shape];
+        Global.main.addChild(this._shape);
+    }
+
+    private createParticle(shape, body) {
+        this._shape = new egret.Shape();
+        this._shape.graphics.lineStyle(1, Color.FILL);
+        this._shape.graphics.beginFill(Color.PARTICLE, 1);
+        this._shape.graphics.drawCircle(0, 0, 2);
+        this._shape.graphics.endFill();
+        body.position[0] /= this._factor;
+        body.position[1] /= this._factor;
+        body.displays = [this._shape];
+        Global.main.addChild(this._shape);
+    }
+
+    private createCapsule(shape, body) {
+        this._shape = new egret.Shape();
+        this._shape.graphics.lineStyle(1, Color.FILL);
+        this._shape.graphics.beginFill(Color.CAPSULE, 1);
+        this._shape.graphics.drawRect(0, 0, (shape.length - 2 * shape.radius) * this._factor, shape.radius * 2 * this._factor);
+        this._shape.graphics.drawCircle(0, shape.radius * this._factor, shape.radius * this._factor);
+        this._shape.graphics.drawCircle((shape.length - shape.radius * 2) * this._factor, shape.radius * this._factor, shape.radius * this._factor);
+        this._shape.graphics.endFill(); 
         this._shape.anchorOffsetX = this._shape.width / 2;
         this._shape.anchorOffsetY = this._shape.height / 2;
         body.position[0] /= this._factor;
--- src/demo/p2demo/PersonalDebugDraw.ts
+++ src/demo/p2demo/PersonalDebugDraw.ts
@@ -3,13 +3,19 @@ class PersonalDebugDraw extends BaseClass {
     private _world: p2.World;
     private _planeBody: p2.Body;
     private _circleBody: p2.Body;
+    private _boxBody: p2.Body;
     private _lineBody: p2.Body;
+    private _particleBody: p2.Body;
+    private _capsuleBody: p2.Body;
     constructor() {
         super();
     }
 
     public init() {
         this.createScene();
+        this.initBottomPlane();
+        this.initLeftGround();
+        this.initRightGround();
         this.initBox(Global.stage.stageWidth / 2, Global.stage.stageHeight * 0.5);
         this.createDebugDraw();
 
@@ -20,19 +26,45 @@ class PersonalDebugDraw extends BaseClass {
     private createScene() {
         this._world = new p2.World();
         this._world.sleepMode = p2.World.BODY_SLEEPING;
+    }
 
+    //平面形状 Plane
+    private initBottomPlane() {
         let planeShape = new p2.Plane();
         this._planeBody = new p2.Body({
             type: p2.Body.STATIC,
             //TODO 地面的位置，这个问题还需要优化
             position: [0, Global.stage.stageHeight * 0.1]
         });
-
         this._planeBody.addShape(planeShape);
         this._world.addBody(this._planeBody);
     }
 
-    private _boxBody: p2.Body;
+    private initLeftGround() {
+        let planeShape = new p2.Box({ width: 0.5, height: 50 });
+        let planeBody = new p2.Body({
+            mass: 1,
+            type: p2.Body.STATIC,
+            //TODO 地面的位置，这个问题还需要优化
+            position: [0, 0]
+        });
+        planeBody.addShape(planeShape);
+        this._world.addBody(planeBody);
+    }
+
+    private initRightGround() {
+        let planeShape = new p2.Box({ width: 0.5, height: 50 });
+        let planeBody = new p2.Body({
+            mass: 1,
+            type: p2.Body.STATIC,
+            //TODO 地面的位置，这个问题还需要优化
+            position: [Global.stage.stageWidth, 0]
+        });
+        planeBody.addShape(planeShape);
+        this._world.addBody(planeBody);
+    }
+
+    //矩形 Box
     private initBox(x, y) {
         let boxShape = new p2.Box({ width: 2, height: 1 });
         this._boxBody = new p2.Body({
@@ -46,6 +78,7 @@ class PersonalDebugDraw extends BaseClass {
         this._world.addBody(this._boxBody);
     }
 
+    //圆形 Circle
     private initCircle(x, y) {
         let circelShape = new p2.Circle({ redius: 0.7 });
         this._circleBody = new p2.Body({
@@ -56,6 +89,7 @@ class PersonalDebugDraw extends BaseClass {
         this._world.addBody(this._circleBody);
     }
 
+    //线形 Line
     private initLine(x, y) {
         let lineShape = new p2.Line({ length: 1.6 });
         this._lineBody = new p2.Body({
@@ -67,15 +101,44 @@ class PersonalDebugDraw extends BaseClass {
         this._world.addBody(this._lineBody);
     }
 
+    //粒子形状 particle
+    private initParticle(x, y) {
+        let particleShape: p2.Particle = new p2.Particle();
+        this._particleBody = new p2.Body({
+            mass: 1,
+            angularVelocity: 1,
+            position: [x, y]
+        });
+        this._particleBody.addShape(particleShape);
+        this._world.addBody(this._particleBody);
+    }
+
+    //胶囊形状 capsule
+    private initCapsule(x, y) {
+        let capsuleShape: p2.Capsule = new p2.Capsule({ length: 2, radius: 0.4 });
+        this._capsuleBody = new p2.Body({
+            mass: 1,
+            angularVelocity: 1,
+            position: [x, y]
+        })
+        this._capsuleBody.addShape(capsuleShape);
+        this._world.addBody(this._capsuleBody);
+    }
+
     private onClickHandler(e: egret.TouchEvent) {
         //TODO 点击屏幕的位置，这个问题也需要处理
-        if (Math.random() > 0.7) {
-            this.initBox(e.stageX, Global.stage.stageHeight - e.stageY);
-        } else if (Math.random() > 0.3 && Math.random() < 0.7) {
-            this.initLine(e.stageX, Global.stage.stageHeight - e.stageY);
-        } else {
-            this.initCircle(e.stageX, Global.stage.stageHeight - e.stageY);
-        }
+        // if (Math.random() <= 0.2) {
+        //     this.initBox(e.stageX, Global.stage.stageHeight - e.stageY);
+        // } else if (Math.random() > 0.2 && Math.random() <= 0.4) {
+        //     this.initLine(e.stageX, Global.stage.stageHeight - e.stageY);
+        // } else if (Math.random() > 0.4 && Math.random() <= 0.6) {
+        //     this.initCircle(e.stageX, Global.stage.stageHeight - e.stageY);
+        // } else if (Math.random() > 0.6 && Math.random() <= 0.8) {
+        //     this.initParticle(e.stageX, Global.stage.stageHeight - e.stageY);
+        // } else if (Math.random() > 0.8) {
+        //     this.initCapsule(e.stageX, Global.stage.stageHeight - e.stageY);
+        // }
+        this.initCapsule(e.stageX, Global.stage.stageHeight - e.stageY);
     }
 
     private _debugDraw: DebugDraw;
